402a4a83d87ffd6c3be3ec56e355f5a7bae26770
xiewenxiu <1840565983@qq.com>
2018-12-29 14:03:40 +0800

    第一次提交测试的

---
+++ src/main/java/com/example/demotwo/DemotwoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotwoApplication.class, args);
+    }
+
+}
+
